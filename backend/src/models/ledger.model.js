a083393cfe369770e9dc1dceb03ebf07250f8a18
sabhi-manu <sabhimanu707@gmail.com>
2026-04-21 10:59:12 +0530

    ledger

---
+++ backend/src/models/ledger.model.js
@@ -0,0 +1,49 @@
+import mongoose  from "mongoose";
+
+const ledgerSchema = new mongoose.Schema({
+    account:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'account',
+        requied:[true,"Account requied for ledger."],
+        index:true,
+        immutable:true
+    },
+    ammount:{
+        type:Number,
+        requied:[true,"Amount is requied for ledger entry."],
+        immutable:true,
+    },
+    type:{
+        type:String,
+        enum:{
+            value:["CREDIT","DEBIT"]
+        },
+        immutable:true,
+        requied:true
+    },
+    transaction:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"transaction",
+        requied:[true,"Transaction required for Ledger."],
+        index:true,
+        immutable:true
+    }
+},{timestamps:true})
+
+
+function preventLedgerModification (){
+    throw new Error("Ledger entries are immutable and cannot be modified or deleted.")
+}
+
+ledgerSchema.pre("findOneAndUpdate",preventLedgerModification);
+ledgerSchema.pre("findOneAndDelete",preventLedgerModification);
+ledgerSchema.pre("findOneAndReplace",preventLedgerModification);
+ledgerSchema.pre("deleteMany",preventLedgerModification);
+ledgerSchema.pre("deleteOne",preventLedgerModification);
+ledgerSchema.pre("updateMany",preventLedgerModification);
+ledgerSchema.pre("updateOne",preventLedgerModification);
+ledgerSchema.pre("update",preventLedgerModification);       
+
+const ledgerModel = mongoose.model("ledger",ledgerSchema)
+
+export default ledgerModel
