ded297c1f0f03d53fbd238ab412f12c45a633aa7
sabhi-manu <sabhimanu707@gmail.com>
2026-05-30 22:22:41 +0530

    cors  rul

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,46 +3,48 @@ import jwt from "jsonwebtoken"
 import emailService from "../utils/mails/emails.js"
 import tokenBlackListModel from "../models/blackList.model.js"
 
-async function userRegisterController(req,res,next) {
+async function userRegisterController(req, res, next) {
     try {
-        const {email,password,userName,systemUser=false,phoneNumber} = req.body
+        const { email, password, userName, systemUser = false, phoneNumber } = req.body
 
-        const isUser = await UserModel.findOne({email})
+        const isUser = await UserModel.findOne({ email })
 
-        if(isUser){
+        if (isUser) {
             return res.status(422).json({
-                message:"User  already Exist With Eamil",
-                status:"failed"
+                message: "User  already Exist With Eamil",
+                status: "failed"
             })
         }
 
         const user = await UserModel.create({
-            email,password,userName,systemUser,phoneNumber
+            email, password, userName, systemUser, phoneNumber
         })
 
-        const token = await jwt.sign({userId:user._id},process.env.JWT_SECRET)
-        res.cookie("token",token , {
-            httpOnly : true,
-             maxAge:  25 * 60 * 1000,
+        const token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 25 * 60 * 1000,
         })
 
 
-    await emailService.sendRegisterEmail({
-            userName:user.userName,
-            email:user.email
+        await emailService.sendRegisterEmail({
+            userName: user.userName,
+            email: user.email
         })
 
-       return res.status (201).json({
-            message:"user register successfully.",
-            status:"success",
-            user:{
-                _id:user._id,
-                email:user.email,
-                userName:user.userName,
-                phoneNumber:user.phoneNumber,
-                updatedAt:user.updatedAt,
-                createdAt:user.createdAt,
-                systemUser:user.systemUser
+        return res.status(201).json({
+            message: "user register successfully.",
+            status: "success",
+            user: {
+                _id: user._id,
+                email: user.email,
+                userName: user.userName,
+                phoneNumber: user.phoneNumber,
+                updatedAt: user.updatedAt,
+                createdAt: user.createdAt,
+                systemUser: user.systemUser
             },
             token
 
@@ -51,49 +53,51 @@ async function userRegisterController(req,res,next) {
         console.log("user Register failed.")
         next(error)
     }
-    
+
 }
 
 
 
-async function userLoginController(req,res,next) {
+async function userLoginController(req, res, next) {
     try {
-        const {email,password} = req.body
-        
-        const user = await UserModel.findOne({email})
-        if(!user) {
-              return res.status(401).json({
-                message:"User  Not Exist .",
-                status:"failed"
+        const { email, password } = req.body
+
+        const user = await UserModel.findOne({ email })
+        if (!user) {
+            return res.status(401).json({
+                message: "User  Not Exist .",
+                status: "failed"
             })
         }
 
         const isPassword = await user.comparePassword(password)
 
-        if(!isPassword){
-              return res.status(404).json({
-                message:"User Detals Not valid .",
-                status:"failed"
+        if (!isPassword) {
+            return res.status(404).json({
+                message: "User Detals Not valid .",
+                status: "failed"
             })
         }
 
-          const token = await jwt.sign({userId:user._id},process.env.JWT_SECRET)
-        res.cookie("token",token , {
-            httpOnly : true,
-             maxAge:  25 * 60 * 1000,
+        const token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 25 * 60 * 1000,
         })
 
         return res.status(200).json({
-            message:"User Login successfully.",
-            status:"success",
-            user:{
-                _id:user._id,
-                email:user.email,
-                userName:user.userName,
-                phoneNumber:user.phoneNumber,
-                updatedAt:user.updatedAt,
-                createdAt:user.createdAt,
-                systemUser:user.systemUser
+            message: "User Login successfully.",
+            status: "success",
+            user: {
+                _id: user._id,
+                email: user.email,
+                userName: user.userName,
+                phoneNumber: user.phoneNumber,
+                updatedAt: user.updatedAt,
+                createdAt: user.createdAt,
+                systemUser: user.systemUser
             },
             token
 
@@ -102,18 +106,18 @@ async function userLoginController(req,res,next) {
     } catch (error) {
         console.log("Error in login .")
         next(error)
-        
+
     }
 }
 
 
-async function userLogoutController(req,res,next) {
+async function userLogoutController(req, res, next) {
     try {
-        const token = req.cookies.token ||  req.headers.authorization?.split(" ")[1]
+        const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
 
-        if(!token){
+        if (!token) {
             return res.status(400).json({
-                messsage:"user logged out ."
+                messsage: "user logged out ."
             })
         }
         await tokenBlackListModel.create({
@@ -122,37 +126,37 @@ async function userLogoutController(req,res,next) {
 
         await res.clearCookie("token")
         return res.status(200).json({
-            message:'user logout successfully.',
-            statue:"success."
+            message: 'user logout successfully.',
+            statue: "success."
         })
     } catch (error) {
-         console.log("Error in Logout .")
+        console.log("Error in Logout .")
         next(error)
-         
+
     }
-    
+
 }
 
-async function getCurrentUserController(req,res,next) {
+async function getCurrentUserController(req, res, next) {
     const user = req.user
     const token = req.token
-    console.log("Current user ",user)
-    console.log("Current user token ",token)
-     return res.status(200).json({
-            message:"User Login successfully.",
-            status:"success",
-            user:{
-                _id:user._id,
-                email:user.email,
-                userName:user.userName,
-                phoneNumber:user.phoneNumber,
-                updatedAt:user.updatedAt,
-                createdAt:user.createdAt,
-                systemUser:user.systemUser
-            },
-            token
+    console.log("Current user ", user)
+    console.log("Current user token ", token)
+    return res.status(200).json({
+        message: "User Login successfully.",
+        status: "success",
+        user: {
+            _id: user._id,
+            email: user.email,
+            userName: user.userName,
+            phoneNumber: user.phoneNumber,
+            updatedAt: user.updatedAt,
+            createdAt: user.createdAt,
+            systemUser: user.systemUser
+        },
+        token
 
-        })
+    })
 }
 
 export default {
